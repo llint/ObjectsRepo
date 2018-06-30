c7d747d1c93b98e9810f3b72f3989fd0c98772f5
Lin Luo <l.luo-pi@outlook.com>
2018-06-29 21:22:46 -0700

    Initial Commit

---
+++ ObjectsRepo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ObjectsRepo
+//
+//  Created by Lin Luo on 6/29/18.
+//  Copyright © 2018 Linfinity. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
